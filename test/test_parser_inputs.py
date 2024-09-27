792e2acccd131694523095d777c6b7f5e7f2eb2f
Fede Sandoval <federico.sandoval@alu.unq.edu.ar>
2024-09-27 01:01:52 -0300

    :white_check_mark: fix paths tests inputs

--- test/test_parser_inputs.py
+++ test/test_parser_inputs.py
@@ -9,16 +9,16 @@ class TestParser(unittest.TestCase):
 
     def setUp(self):
         self.parser = Parser()
+        self.input_dir = os.path.join(os.path.dirname(__file__), 'inputs')
 
     def test_el_analizador_lexico_genera_los_ast_esperados(self):
-        input_dir = 'inputs'
-        test_cases = [f for f in os.listdir(input_dir) if f.endswith('.input')]
+        test_cases = [f for f in os.listdir(self.input_dir) if f.endswith('.input')]
 
         for test_case in test_cases:
             with self.subTest(test_case=test_case):
                 base_name = test_case.split('.')[0]
-                flecha_input_file = os.path.join(input_dir, f'{base_name}.input')
-                expected_json_file = os.path.join(input_dir, f'{base_name}.expected')
+                flecha_input_file = os.path.join(self.input_dir, f'{base_name}.input')
+                expected_json_file = os.path.join(self.input_dir, f'{base_name}.expected')
 
                 with open(expected_json_file, 'r') as f:
                     expected_ast = json.dumps(json.load(f), separators=(',', ':'))
